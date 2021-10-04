29cd5a8e0a6e76af010448554037e5759c7fd917
Elvis Offor <eoffor21@gmail.com>
2021-10-04 11:46:31 -0700

    Fix module path in setup.py

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     keywords="science chemistry molecular biology",
     entry_points={
         "console_scripts": [
-            "pdb2pqr30=pdb2pqr.main:main",
+            "pdb2pqr30=pdb2pqr.pdb2pqr:main",
             "dx2cube=pdb2pqr.main:dx_to_cube",
             "psize=pdb2pqr.psize:main",
             "inputgen=pdb2pqr.inputgen:main",
