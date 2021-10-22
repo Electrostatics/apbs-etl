ee801ddf4ad48f01876471dc5e5f94c2f4c81888
Elvis Offor <eoffor21@gmail.com>
2021-10-22 16:20:03 -0700

    Change expected stings to raw strings

--- tests/test_elec.py
+++ tests/test_elec.py
@@ -17,7 +17,29 @@ from re import sub
             False,
             0.0,
             False,
-            "elec mg-auto dime 0 0 0 cglen 0.0000 0.0000 0.0000 fglen 0.0000 0.0000 0.0000 cgcent mol 1 fgcent mol 1 mol 1 lpbe bcfl sdh pdie 2.0000 sdie 78.5400 srfm smol chgm spl2 sdens 10.00 srad 1.40 swin 0.30 temp 298.15 calcenergy total calcforce no write pot dx pot end",
+            r"""elec 
+    mg-auto
+    dime 0 0 0
+    cglen 0.0000 0.0000 0.0000
+    fglen 0.0000 0.0000 0.0000
+    cgcent mol 1
+    fgcent mol 1
+    mol 1
+    lpbe
+    bcfl sdh
+    pdie 2.0000
+    sdie 78.5400
+    srfm smol
+    chgm spl2
+    sdens 10.00
+    srad 1.40
+    swin 0.30
+    temp 298.15
+    calcenergy total
+    calcforce no
+    write pot dx pot
+end
+""",
             id="1 mg-auto False 0.0 False",
         ),
         pytest.param(
@@ -26,16 +48,63 @@ from re import sub
             False,
             0.0,
             True,
-            "elec mg-auto dime 0 0 0 cglen 0.0000 0.0000 0.0000 fglen 0.0000 0.0000 0.0000 cgcent mol 1 fgcent mol 1 mol 1 lpbe bcfl sdh pdie 2.0000 sdie 78.5400 srfm smol chgm spl2 sdens 10.00 srad 1.40 swin 0.30 temp 298.15 calcenergy total calcforce no write pot dx test2 end",
+            r"""elec 
+    mg-auto
+    dime 0 0 0
+    cglen 0.0000 0.0000 0.0000
+    fglen 0.0000 0.0000 0.0000
+    cgcent mol 1
+    fgcent mol 1
+    mol 1
+    lpbe
+    bcfl sdh
+    pdie 2.0000
+    sdie 78.5400
+    srfm smol
+    chgm spl2
+    sdens 10.00
+    srad 1.40
+    swin 0.30
+    temp 298.15
+    calcenergy total
+    calcforce no
+    write pot dx test2
+end
+""",
             id="2 mg-auto False 0.0 True",
         ),
         pytest.param(
             "test3.pqr",
             "mg-para",
-            False,
+            True,
             0.0,
             True,
-            "elec mg-para dime 0 0 0 pdime 0 0 0 ofrac 0.1 cglen 0.0000 0.0000 0.0000 fglen 0.0000 0.0000 0.0000 cgcent mol 1 fgcent mol 1 mol 1 lpbe bcfl sdh pdie 2.0000 sdie 78.5400 srfm smol chgm spl2 sdens 10.00 srad 1.40 swin 0.30 temp 298.15 calcenergy total calcforce no write pot dx test3 end",
+            r"""elec 
+    mg-para
+    dime 0 0 0
+    pdime 0 0 0
+    ofrac 0.1
+    cglen 0.0000 0.0000 0.0000
+    fglen 0.0000 0.0000 0.0000
+    cgcent mol 1
+    fgcent mol 1
+    async 0
+    mol 1
+    lpbe
+    bcfl sdh
+    pdie 2.0000
+    sdie 78.5400
+    srfm smol
+    chgm spl2
+    sdens 10.00
+    srad 1.40
+    swin 0.30
+    temp 298.15
+    calcenergy total
+    calcforce no
+    write pot dx test3
+end
+""",
             id="3 mg-para False 0.0 True",
         ),
         pytest.param(
@@ -44,7 +113,27 @@ from re import sub
             False,
             0.0,
             True,
-            "elec mg-manual dime 0 0 0 glen 0.000 0.000 0.000 gcent mol 1 mol 1 lpbe bcfl sdh pdie 2.0000 sdie 78.5400 srfm smol chgm spl2 sdens 10.00 srad 1.40 swin 0.30 temp 298.15 calcenergy total calcforce no write pot dx test4 end",
+            r"""elec 
+    mg-manual
+    dime 0 0 0
+    glen 0.000 0.000 0.000
+    gcent mol 1
+    mol 1
+    lpbe
+    bcfl sdh
+    pdie 2.0000
+    sdie 78.5400
+    srfm smol
+    chgm spl2
+    sdens 10.00
+    srad 1.40
+    swin 0.30
+    temp 298.15
+    calcenergy total
+    calcforce no
+    write pot dx test4
+end
+""",
             id="4 mg-manual False 0.0 True",
         ),
     ],
@@ -54,8 +143,7 @@ def test_elec(pqr_name, method, asyncflag, istrng, potdx, output):
     elec = Elec(pqr_name, method, Psize(), asyncflag, istrng, potdx)
     print(elec)
 
-    # Replace all newlines and multiple whitespaces with a single space
-    assert sub("\s+", " ", str(elec).replace("\n", " ").strip()) == output
+    assert str(elec) == output
 
 
 def test_invalid_calctype():
