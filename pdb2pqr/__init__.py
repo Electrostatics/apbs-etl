256029385016dc119fb5460de555e32937ae458b
Elvis Offor <eoffor21@gmail.com>
2021-11-29 10:08:51 -0800

    Removed unused FADD in __init__.py

--- pdb2pqr/__init__.py
+++ pdb2pqr/__init__.py
@@ -10,13 +10,11 @@ from sys import version_info
 from ._version import __version__  # noqa: F401
 from .pdb2pqr import main
 
-#: The number of Ångströms added to the molecular dimensions to determine the
-#: find grid dimensions
-FADD = 20.0
 
 _LOGGER = logging.getLogger(__name__)
 
 
+# TODO: update minimum required version to 3.7
 assert version_info >= (3, 5)
 logging.captureWarnings(True)
 
